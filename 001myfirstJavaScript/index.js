a7da51aeb060b30e9ca87488989f305e99e172ed
MoniqueDrost <80634091+MoniqueDrost@users.noreply.github.com>
2021-05-05 13:16:51 +0200

    Add files via upload

---
+++ 001myfirstJavaScript/index.js
@@ -0,0 +1,17 @@
+console.log("Hello Winc Academy")
+
+let name = 'Monique';
+
+console.log(name)
+
+let years = "4";
+
+console.log(years + years)
+
+let age = 40;
+
+console.log(typeof number)
+
+let agetext = "40";
+
+console.log(typeof string)
